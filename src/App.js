c2c85f6616e2ef9c436d2bef28ee85cb4bce8157
Roman Pavliv <roman.pawliw@gmail.com>
2017-09-20 12:12:29 +0300

    add redux file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux';
+import { Input, Button } from 'semantic-ui-react'
 import './App.css';
 
+import {  
+  getCity,
+  getWeatherCurrentUserPosition,
+} from './redux';
+
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.getWeatherCurrentUserPosition(this, true);
+  }
+
   render() {
     return (
       <div className="App">
+        <h1>Get the weather!</h1>
+         <form>
+            <Input type="text" id="city-enter" placeholder="enter city" name="city"
+                   onChange={(event) => this.props.getCity(event)} />
+
+            <Button type="button" primary onClick={() => {
+                    this.props.getWeatherCurrentUserPosition(this)}}>Search</Button>
+        </form>
+        <section id="weather-text" name="weather">The weather in: {this.props.geod.weather} </section>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => ({  
+  geod: state.geod
+});
+
+const mapDispatchToProps = { 
+    getCity,
+    getWeatherCurrentUserPosition,
+};
+
+const AppContainer = connect(  
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
+
+export default AppContainer;
---
+++ src/redux.js
@@ -0,0 +1,96 @@
+import {  
+  applyMiddleware,
+  combineReducers,
+  createStore,
+} from 'redux';
+
+import axios from 'axios';
+import thunk from 'redux-thunk';
+
+const initialState = {city: '', weather: ''};
+
+export const getWeatherCurrentUserPosition = (self, currentPosition) => {
+  return dispatch => {
+    
+    function getWeather(self)  {
+      let url = `https://api.openweathermap.org/data/2.5/weather/?q=${self.props.geod.city}
+                 &units=metric&APPID=1feb720412a26a7828127770f514bf58`;
+      
+      axios.get(url)
+      .then(res => {
+        let weather = self.props.geod.city + ", " + res.data.sys.country + " " + res.data.main.temp +  " " +
+                      String.fromCharCode(176) + "C, " + res.data.weather[0].description + ", wind: " + 
+                      res.data.wind.speed + " m/s";
+  
+        dispatch({type: 'GET_WEATHER', payload: weather});
+      })
+      .catch(err => {
+        console.log(err);
+      });     
+    }
+
+    if (!currentPosition) {
+      getWeather(self);
+    }
+    else {
+
+      if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(position => {
+
+          let googleApiURL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},
+                              ${position.coords.longitude}&language=en&key=AIzaSyAlI3lat_5W-O4wWZ0p1peRh6vFCYeD89I`;
+          
+          axios.get(googleApiURL)
+          .then(res => {
+
+            dispatch({type: 'GET_CITY', payload: res.data.results[0].address_components[3].long_name});
+          
+            let url = `https://api.openweathermap.org/data/2.5/weather/?q=${self.props.geod.city} 
+                      &units=metric&APPID=1feb720412a26a7828127770f514bf58`;
+
+            getWeather(self);
+
+          })
+          .catch(err => {
+            console.log(err);
+          });
+        });
+      } 
+      else {
+        dispatch({type: 'GET_WEATHER', payload: 'geolocation is not allowed'});
+      }
+    }
+  }
+}
+
+export const getCity = name => {
+  return dispatch => {
+    dispatch({type: 'GET_CITY', payload: name.target.value});
+  }
+}
+
+export const geod = (state = initialState, action) => {
+  switch (action.type) {
+    case 'GET_WEATHER':
+      return  Object.assign({}, {weather: action.payload});
+    case 'GET_CITY':
+      return Object.assign({}, {city: action.payload});
+    default:
+      return state;
+  }
+};
+
+export const reducers = combineReducers({  
+  geod,
+});
+
+export function configureStore(initialState = {}) {  
+  const store = createStore(
+    reducers,
+    initialState,
+    applyMiddleware(thunk)
+  )
+  return store;
+};
+  
+export const store = configureStore();  
