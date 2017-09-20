89435f071694e0be0d13414465a795626269abfd
Roman Pavliv <roman.pawliw@gmail.com>
2017-09-20 23:05:47 +0300

    cleared code

--- src/App.js
+++ src/App.js
@@ -16,17 +16,17 @@ class App extends Component {
 
   render() {
 
-  let weather = this.props.geod.weather ? (this.props.geod.weather ? this.props.geod.weather : <Loader active /> ) : 
+    let weather = this.props.geod.weather ? (this.props.geod.weather ? this.props.geod.weather : <Loader active />) : 
                 (window.sessionStorage.getItem("weather") ? "" : <Loader active />);
     return (
       <div className="App">
         <Header size="huge">Get the weather!</Header>
          <form>
             <Input type="text" id="city-enter" placeholder="enter city" name="city"
-                   onChange={(event) => this.props.getCity(event)} />
+                   onChange={(e) => this.props.getCity(e)} />
 
-            <Button type="button" primary onClick={() => {
-                    this.props.getWeatherCurrentUserPosition(this)}}>Search</Button>
+            <Button type="button" primary 
+                   onClick={() => {this.props.getWeatherCurrentUserPosition(this)}}>Search</Button>
         </form>
         <Header size="medium" id="weather-text">
            {"The weather in: " + weather}
@@ -36,13 +36,13 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = (state) => ({  
+const mapStateToProps = state => ({  
   geod: state.geod
 });
 
 const mapDispatchToProps = { 
-    getCity,
-    getWeatherCurrentUserPosition,
+  getCity,
+  getWeatherCurrentUserPosition,
 };
 
 const AppContainer = connect(  
